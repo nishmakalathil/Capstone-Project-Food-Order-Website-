4a2ad1324648f111107efac7a1f1aafc2498ab9c
NIshma k <anukthanish@gmail.com>
2025-02-16 23:30:08 +0530

    Update axiosInstances.js

--- Frontend/food/src/config/axiosInstances.js
+++ Frontend/food/src/config/axiosInstances.js
@@ -3,6 +3,18 @@ import axios from "axios";
 export const axiosInstance = axios.create({
     baseURL: `${import.meta.env.VITE_BASE_URL}/api`,
     withCredentials: true,
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
+
+// Automatically attach the Authorization header if a token is stored
+axiosInstance.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token"); // Or use cookies
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  });
 
 export default axiosInstance;
